1a2132430a99e663cdc7b8c8a5f88b185471f14b
s68122250048 <s68122250048@ssru.ac.th>
2026-01-07 21:16:12 +0700

    Delete MedicalRecord.java

--- MedicalRecord.java
+++
@@ -1,9 +0,0 @@
-public class MedicalRecord {
-    private String RecordId;
-    private String Diagnosis;
-    private String Treatment;
-
-    public void SaveRecord(){
-        System.out.println("From class MedicalRecord = SaveRecord");
-    }
-}
