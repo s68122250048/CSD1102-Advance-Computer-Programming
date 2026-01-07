69baea15d790cd18473dae36b52965745e38240c
s68122250048 <s68122250048@ssru.ac.th>
2026-01-07 21:17:11 +0700

    Delete Student.java

--- Student.java
+++
@@ -1,9 +0,0 @@
-public class Student {
-    private String Id;
-    private String Name;
-    private String Faculty;
-
-    public void RegisterVisit(){
-        System.out.println("From class Student = RegisterVisit");
-    }
-}
