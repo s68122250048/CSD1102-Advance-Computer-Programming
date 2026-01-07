7fe41b5ce4286ae484e81f9cf9d1d3fc72208b94
s68122250048 <s68122250048@ssru.ac.th>
2026-01-07 21:16:01 +0700

    Delete Doctor.java

--- Doctor.java
+++
@@ -1,9 +0,0 @@
-public class Doctor extends ClinicStaff {
-    public void ExaminePatient(){
-        System.out.println("From class Doctor = ExaminePatient");
-    }
-    public void IssueMedicalCertificate(){
-        System.out.println("From class Doctor = IssueMedicalCertificate");
-    }
-
-}
