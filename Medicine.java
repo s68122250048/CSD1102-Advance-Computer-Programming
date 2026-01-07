c2576d08732c77a79dfd5c92483479a97fc5c2aa
s68122250048 <s68122250048@ssru.ac.th>
2026-01-07 21:16:19 +0700

    Delete Medicine.java

--- Medicine.java
+++
@@ -1,9 +0,0 @@
-public class Medicine {
-    private String Medicineid;
-    private String Name;
-    private String Dosage;
-
-    public void Dispense(){
-        System.out.println("From class Medicine = Dispense");
-    }
-}
