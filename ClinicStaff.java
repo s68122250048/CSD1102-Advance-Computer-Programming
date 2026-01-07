9b6a7ef7d6fd41c6daa8c6daa93b7fc4a5571ea5
s68122250048 <s68122250048@ssru.ac.th>
2026-01-07 21:15:54 +0700

    Delete ClinicStaff.java

--- ClinicStaff.java
+++
@@ -1,8 +0,0 @@
-public class ClinicStaff {
-    private String StaffId;
-    private String Name;
-
-    public void CallQueue() {
-        System.out.println("From class ClinicStaff = CallQueue");
-    }
-}
